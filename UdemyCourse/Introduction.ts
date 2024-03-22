1b40c4a91e759c04f583b44ae8d1d6a83675ea45
Vedat Cinbat <vedat.cinbat@btctrader.com>
2024-03-22 13:17:50 +0300

    Introduction.ts Changed

---
+++ UdemyCourse/Introduction.ts
@@ -0,0 +1,25 @@
+interface Person {
+    name: string;
+    age: number;
+    hobbies: string[];
+}
+
+const message: string = "Hello TypeScript";
+
+console.log(message);
+
+
+
+const hobbies = ["Sports", "Cooking"];
+
+const person : Person = {
+    name: "Max",
+    age: 30,
+    hobbies: hobbies
+};
+
+
+console.log(person)
+
+
+
